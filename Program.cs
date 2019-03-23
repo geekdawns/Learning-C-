aba84a75c5ac23acfccfca2722619d82a88cd22a
geekjhin <geekjhin@gmail.com>
2019-03-23 16:10:38 +0800

    hello world

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
